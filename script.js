6922f772598debdce1656ccddfd1d90c03131266
mohammedkhaled <mohdkhaled1031@gmail.com>
2025-05-26 22:13:40 +0530

    update process bar value if the target is check

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const progressValue = document.querySelector('.progress-value')
 
 const allGoals = JSON.parse(localStorage.getItem('allGoals'))|| {}
 
+let completedGoalsCount = Object.values(allGoals).filter((goal) => goal.completed).length
+
+progressValue.style.width = `${(completedGoalsCount / Object.keys(allGoals).length) * 100}%`
+
+
 checkBoxList.forEach((checkbox) => {
   checkbox.addEventListener('click', (e) => {
     const allGoalsAdded = [...inputFields].every(function (input) {
@@ -14,9 +19,11 @@ checkBoxList.forEach((checkbox) => {
 
     if (allGoalsAdded) {
       checkbox.parentElement.classList.toggle('completed')
-      progressValue.style.width = '33.33%'
       const inputId = checkbox.nextElementSibling.id
       allGoals[inputId].completed = !allGoals[inputId].completed
+      completedGoalsCount = Object.values(allGoals).filter((goal) => goal.completed).length
+      progressValue.style.width = `${(completedGoalsCount / Object.keys(allGoals).length) * 100}%`
+      progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 completed`
       localStorage.setItem('allGoals', JSON.stringify(allGoals))
     } else {
         progressBar.classList.add('show-error')
@@ -28,17 +35,25 @@ inputFields.forEach((input) => {
    input.value = allGoals[input.id].name
 
    if (allGoals[input.id].completed){
-    input.parentElement.classList.add('completed')
+      input.parentElement.classList.add('completed')
    }
 
     input.addEventListener('focus', () => {
-        progressBar.classList.remove('show-error')
+      progressBar.classList.remove('show-error')
     })
+
     input.addEventListener('input',(e)=>{
+     if (allGoals[input.id].completed){
+       input.value= allGoals[input.id].name
+       return
+      }
+
+
       allGoals[input.id] ={
         name:input.value,
         completed:false,
       }
+
       localStorage.setItem('allGoals', JSON.stringify(allGoals))
     })
 
